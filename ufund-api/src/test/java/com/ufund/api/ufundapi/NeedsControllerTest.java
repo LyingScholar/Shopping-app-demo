6bd8a1bb0305e1643720f7fbd72a4af1f24b2088
Michael B <annoyingcracks@gmail.com>
2024-06-19 09:04:01 -0400

    all controller tests done

--- ufund-api/src/test/java/com/ufund/api/ufundapi/NeedsControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/NeedsControllerTest.java
@@ -1,5 +1,8 @@
 package com.ufund.api.ufundapi;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,10 @@ import org.springframework.http.ResponseEntity;
 
 public class NeedsControllerTest {
 
+    //we set up a mock environment for the cupboard before every test
+    //test names are self explanatory so specific jdocs are omitted
+    //response entity and mockito were googled a lot while writing these tests
+
     @Mock
     private Cupboard cupboard;
     @InjectMocks
@@ -22,7 +29,7 @@ public class NeedsControllerTest {
     }
 
     @Test
-    void testgetNeed_shouldReturnTheNeedIfNeedExists() {
+    void testGetNeed_shouldReturnTheNeedIfNeedExists() {
         int id = 1;
         Need need = new Need(id, "Test Need", 10, 100, "Food");
         when(cupboard.getNeed(id)).thenReturn(need);
@@ -36,7 +43,7 @@ public class NeedsControllerTest {
 
 
     @Test
-    void testgetNeed_NeedDoesNotExist() {
+    void testGetNeed_NeedDoesNotExist() {
         int id = 1;
         when(cupboard.getNeed(id)).thenReturn(null);
 
@@ -45,5 +52,63 @@ public class NeedsControllerTest {
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
+    @Test
+    void testGetNeds() {
+        Need[] needs = {new Need(1, "Need 1", 10, 100, "Food"),
+                        new Need(2, "Need 4", 20, 200, "Non-Fod")};
+        when(cupboard.getNeeds()).thenReturn(needs);
+
+        ResponseEntity<Need[]> response = needController.getNeeds();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(Arrays.asList(needs), Arrays.asList(response.getBody()));
+    }
+
+
+
+    @Test
+    void testSearchNeeds() {
+        Need[] needs = {new Need(1, "Test Need", 10, 100, "Food"),
+                        new Need(2, "Taste Need", 20, 269, "Non-Fod")};
+        when(cupboard.findNeeds("Test")).thenReturn(needs);
+
+        ResponseEntity<Need[]> response = needController.searchNeeds("Test");
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(Arrays.asList(needs), Arrays.asList(response.getBody()));
+    }
+
+    @Test
+    void testcreateNewNeed() throws IOException {
+         Need newNeed = new Need(1, "New Need", 30, 300, "Food");
+        when(cupboard.createNeed(newNeed)).thenReturn(newNeed);
+
+        ResponseEntity<Need> response = needController.createNeed(newNeed);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        assertEquals(newNeed, response.getBody());
+    }
+
+    @Test
+    void testupdateNeed_NeedExists() throws IOException{
+         Need updatedNeed = new Need(1, "Updatee Need", 43, 400, "Non-Fod");
+        when(cupboard.updateNeed(updatedNeed)).thenReturn(updatedNeed);
+
+        ResponseEntity<Need> response = needController.updateNeed(updatedNeed);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(updatedNeed, response.getBody());
+    }
+
+    @Test
+    void testupdateNeed_NeedDosntExist() throws IOException {
+         Need updatedNeed = new Need(1, "Updatee Need", 43, 400, "Non-Fod");
+        when(cupboard.updateNeed(updatedNeed)).thenReturn(null);
+
+        ResponseEntity<Need> response = needController.updateNeed(updatedNeed);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
 
 }
