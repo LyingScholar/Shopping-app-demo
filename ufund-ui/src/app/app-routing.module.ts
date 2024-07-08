6eecdc30ab744451b5c03a5bc2032cb2affb469b
Quinton Miller <qjm412@gmail.com>
2024-07-08 15:44:00 -0400

    Home-Page back merged with main, setting up login page.

--- ufund-ui/src/app/app-routing.module.ts
+++ ufund-ui/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home-page', pathMatch: 'full' },
-  { path: 'home-page', component: HomePageComponent }
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'login-page', component: LoginPageComponent }
 ]
 
 
--- ufund-ui/src/app/app.routes.ts
+++ ufund-ui/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home-page', pathMatch: 'full' },
-  { path: 'home-page', component: HomePageComponent }
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'login-page', component: LoginPageComponent }
 ]
