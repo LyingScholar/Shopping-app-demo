227649db5a10858e34ce139f158e4e3d1d1ff522
Quinton Miller <qjm412@gmail.com>
2024-07-16 05:53:01 -0400

    basket should work but running into a weird login error.

--- ufund-ui/src/app/mini-need-basket/need-preview/need-preview.component.ts
+++ ufund-ui/src/app/mini-need-basket/need-preview/need-preview.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Need } from '../../needs-page/needs-list/need';
+import { RemoveFromBasketButtonComponent } from './remove-from-basket-button/remove-from-basket-button.component';
 
 @Component({
   selector: 'app-need-preview',
   standalone: true,
-  imports: [],
+  imports: [RemoveFromBasketButtonComponent],
   templateUrl: './need-preview.component.html',
   styleUrl: './need-preview.component.css'
 })
 export class NeedPreviewComponent {
-
+  @Input() need!: Need;
 }
---
+++ ufund-ui/src/app/mini-need-basket/need-preview/remove-from-basket-button/remove-from-basket-button.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Need } from '../../../needs-page/needs-list/need';
+import { LinloutService } from '../../../linlout.service';
+import { UserService } from '../../../user.service';
+import { User } from '../../../user';
+
+@Component({
+  selector: 'app-remove-from-basket-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './remove-from-basket-button.component.html',
+  styleUrl: './remove-from-basket-button.component.css'
+})
+export class RemoveFromBasketButtonComponent {
+  @Input() need!: Need;
+  user: User = new class {
+    id: number = 0;
+    name: string = "";
+    admin: boolean = false;
+  };
+
+  constructor (private linloutService: LinloutService,private userService: UserService) {
+    this.linloutService.user$.subscribe(user => {
+      this.user = user;
+    })
+  }
+
+  removeFromBasket(): void {
+    this.userService.removeNeed(this.user.id,this.need.id);
+    window.location.reload()
+  }
+}
