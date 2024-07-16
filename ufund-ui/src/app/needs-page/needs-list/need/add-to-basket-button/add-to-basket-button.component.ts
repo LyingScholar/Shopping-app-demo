eb2a0d7d637d074d957dbc7966db17d16420c2fc
Quinton Miller <qjm412@gmail.com>
2024-07-16 00:58:18 -0400

    preparing for new functions in sevice files to be added. code for needs page progressed.

--- ufund-ui/src/app/needs-page/needs-list/need/add-to-basket-button/add-to-basket-button.component.ts
+++ ufund-ui/src/app/needs-page/needs-list/need/add-to-basket-button/add-to-basket-button.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Need } from '../../need';
+import { NeedService } from '../../../../need.service';
+import { UserService } from '../../../../user.service';
 
 @Component({
   selector: 'app-add-to-basket-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-to-basket-button.component.html',
   styleUrl: './add-to-basket-button.component.css'
 })
 export class AddToBasketButtonComponent {
+
+  private need: Need;
+  private needService: NeedService;
+  private userService: UserService;
+
+  constructor (need: Need,needService: NeedService,userService: UserService) {
+    this.need = need;
+    this.needService = needService;
+    this.userService = userService;
+  }
+
+  addToBasket(): void {
+    //call function to remove need from all needs
+    //call function to add need to user's basket
+    window.location.reload()//refresh page
+    return 3; //throwing error so I don't forget to add these functions later
+  }
 
 }
