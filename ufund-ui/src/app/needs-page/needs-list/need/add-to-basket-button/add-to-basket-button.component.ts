626d4ec587a74449e1b4110d7558590edc929ea0
Quinton Miller <qjm412@gmail.com>
2024-07-15 23:12:25 -0400

    progress towards viewing needs page. Buttons next.

---
+++ ufund-ui/src/app/needs-page/needs-list/need/add-to-basket-button/add-to-basket-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-to-basket-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-to-basket-button.component.html',
+  styleUrl: './add-to-basket-button.component.css'
+})
+export class AddToBasketButtonComponent {
+
+}
